fcf98b8b6bc2e637050385425204872423427cc0
Sinner <64489962+SinnerK0N@users.noreply.github.com>
2022-11-20 19:48:03 +0100

    Handle possible crash when parsing from YouTube

--- threads/looped/youtube.js
+++ threads/looped/youtube.js
@@ -1,6 +1,7 @@
 const rss = require("rss-converter");
 const fs = require("fs");
 const configManager = require("../../config/manager.js");
+const logger = require("../../utils/logger.js");
 
 module.exports = {
     Run: async function() {
@@ -10,17 +11,25 @@ module.exports = {
             let config = cfg;
 
             config.youtube.forEach(async channel => {
-                let rss_feed = await rss.toJson("https://www.youtube.com/feeds/videos.xml?channel_id=" + channel.youtube_id);
-                let json = channel.videos;
-                let posted_vids = JSON.stringify(json);
-                if (!posted_vids.includes(rss_feed.items[0].yt_videoId)) {
-                    json.push(rss_feed.items[0].yt_videoId);
-                    let json_link = JSON.stringify(json);
-                    channel.videos = json;
-                    await configManager.writeConfig("main", config);
+                try
+                {
+                    let rss_feed = await rss.toJson("https://www.youtube.com/feeds/videos.xml?channel_id=" + channel.youtube_id);
+                    let json = channel.videos;
+                    let posted_vids = JSON.stringify(json);
+                    if (!posted_vids.includes(rss_feed.items[0].yt_videoId))
+                    {
+                        json.push(rss_feed.items[0].yt_videoId);
+                        let json_link = JSON.stringify(json);
+                        channel.videos = json;
+                        await configManager.writeConfig("main", config);
 
-                    let composedMsg = "<@&1041750093080051795> <@" + channel.discord_id + "> posted a new video:\n" + rss_feed.items[0].link;
-                    await discordBot.Messaging.sendMsgToChannel("videos", composedMsg);
+                        let composedMsg = "<@&1041750093080051795> <@" + channel.discord_id + "> posted a new video:\n" + rss_feed.items[0].link;
+                        await discordBot.Messaging.sendMsgToChannel("videos", composedMsg);
+                    }
+                }
+                catch(e)
+                {
+                    logger.error("Failed to parse YouTube data for: " + channel.youtube_id);
                 }
             });
         });
